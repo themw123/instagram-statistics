de30f6a1662d08c387f7302e5fc5a0ecda4bed54
themw123 <80266862+themw123@users.noreply.github.com>
2021-09-23 12:04:56 +0200

    data1 und data2 thread -> heavy von data getrennt

--- Instagram/src/Instagram.java
+++ Instagram/src/Instagram.java
@@ -33,6 +33,7 @@ public class Instagram{
 	
 	private APIRequest r;
 	
+	private boolean data1;
 	private String[] following;
 	private String[] followers;
 	private Vector<String> notFollowingYou;
@@ -88,8 +89,10 @@ public class Instagram{
 	
 
 	
-    public void data(){
+    public void data1(){
 		
+    	data1 = true;
+    	
     	//System.out.println("Data1-Thread running");
 		Thread t1 = new Thread(() -> setFollowingAndFollowers("following"));
 		t1.start();
@@ -131,39 +134,11 @@ public class Instagram{
 		t6.start();
 		
 		
-		
-		
     	//System.out.println("Data7-Thread running");
 		Thread t7 = new Thread(() -> setMyPosts());
 		t7.start();
 		
-		//Auf Posts warten
-		try {
-			t7.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		Thread t8 = null;
-		Thread t9 = null;
-		if(following != null && followers != null && myPosts != null) {
-			//System.out.println("\n!!!!!!!!HEAVY!!!!!!!!");
-		    //System.out.println("Data8-Thread running");
-			t8 = new Thread(() -> setMostLikedOrCommentedByFollowers("liker"));
-			t8.start();
-			
-			
-		    //System.out.println("Data9-Thread running");
-			t9 = new Thread(() -> setMostLikedOrCommentedByFollowers("commenter"));
-			t9.start();
-			
-		}
-		
-		
-		
-		
+
 		
 		
 		
@@ -176,20 +151,70 @@ public class Instagram{
     		}
 			t5.join();
 			t6.join();
-    		if(following != null && followers != null && myPosts != null) {
-				t8.join();
-				t9.join();
-		        setDataPoolLog();
-				//System.out.println("!!!!!!!!HEAVY!!!!!!!!\n");
-			}
+			t7.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
     	
-       //Main Thread starten
-		System.out.println("Data-Thread finished");
 	}
 	
+    
+    
+    
+    
+    
+    
+    
+    public void data2() {
+    	//username, sessionId und ds_user_id aus App holen und den constructor übergeben!!!!!
+		//posts und Follower/Following holen ODER posts und Follower/Following aus App holen
+    	if(!data1) {
+        	//System.out.println("Data1-Thread running");
+    		Thread t1 = new Thread(() -> setFollowingAndFollowers("following"));
+    		t1.start();
+    		
+        	//System.out.println("Data2-Thread running");
+    		Thread t2 = new Thread(() -> setFollowingAndFollowers("followers"));
+    		t2.start();
+    		
+    		Thread t7 = new Thread(() -> setMyPosts());
+    		t7.start();
+    		
+			try {
+				t1.join();
+				t2.join();
+				t7.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+    	}
+    	
+    	
+		if(following != null && followers != null && myPosts != null) {
+			//System.out.println("\n!!!!!!!!HEAVY!!!!!!!!");
+		    //System.out.println("Data8-Thread running");
+			Thread t8 = new Thread(() -> setMostLikedOrCommentedByFollowers("liker"));
+			t8.start();
+			
+			
+		    //System.out.println("Data9-Thread running");
+			Thread t9 = new Thread(() -> setMostLikedOrCommentedByFollowers("commenter"));
+			t9.start();
+			
+			
+			try {
+				t8.join();
+				t9.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	        setDataPoolLog();
+			//System.out.println("!!!!!!!!HEAVY!!!!!!!!\n");
+		}
+		
+		
+    }
 	
 	
 	
@@ -526,12 +551,14 @@ public class Instagram{
 		
 		//Maximal 12 Threads laufen gleichzeitig.
 		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(12);
-        if(likerOrCommenter.equals("liker")) {
-        	//System.out.println("Data8pool running");
+        /*
+		if(likerOrCommenter.equals("liker")) {
+        	System.out.println("Data8pool running");
         }
 		else if(likerOrCommenter.equals("commenter")) {
-    		//System.out.println("Data9pool running");
+    		System.out.println("Data9pool running");
         }
+        */
 		for(String post : myPosts) {
             executor.submit(() -> {
             	boolean answer = true;
